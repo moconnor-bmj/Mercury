5a92c66b7ee0fec7a4219c10bca69a466a9cb0ec
Michael OConnor <moconnor@bmj.com>
2019-04-02 17:51:35 +0100

    Hist-Exam; Invetigations (partial)

--- js/main.js
+++ js/main.js
@@ -2719,3 +2719,23 @@ $(function() {
     $('#showalg').toggleClass("btn-secondary", comorbidities.is(":checked"));
   });
 });
+
+// POCE Evidence Accordion (nested)
+var acc = document.getElementsByClassName("evidence-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
